2ed428dd44619be163a0464aca0dbda6ad3b4e76
Mochammad Rizky Juliansyah <suryasadewa2904@gmail.com>
2025-10-08 11:41:24 +0700

    pembenaran namespace finance

--- app/Http/Controllers/Finance/PurchaseReturnController.php
+++ app/Http/Controllers/Finance/PurchaseReturnController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Finance;
 
 use App\Http\Controllers\Owner\PurchaseReturnController as BaseController;
 use App\Models\PurchaseReturn;
